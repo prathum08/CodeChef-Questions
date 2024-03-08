8e3e8a26c5cff8909959ee92e9817b5e5e4d3916
prathum08 <prathumbhangadia08@gmail.com>
2024-03-08 20:17:09 +0530

    Question 1

---
+++ 1. Cricket Tournament.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n,m,total;
+	    cin >> n >> m;
+	    total = n -1;
+	    if(total >= m){
+	        cout << "YES" << endl;
+	    }
+	    else{
+	        cout << "NO" << endl;
+	    }
+	}
+
+}
+//Author - Prathum (8/3/24)
